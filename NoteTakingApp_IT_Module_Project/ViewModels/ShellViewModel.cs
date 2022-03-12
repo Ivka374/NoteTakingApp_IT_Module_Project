9d8703329e79b172076cd35730a7c162ebef2f59
Iva Bondzheva <ivka@dsoft-bg.com>
2022-03-12 21:56:06 +0200

    More changes to UI

--- NoteTakingApp_IT_Module_Project/ViewModels/ShellViewModel.cs
+++ NoteTakingApp_IT_Module_Project/ViewModels/ShellViewModel.cs
@@ -5,7 +5,38 @@ using System.Text;
 
 namespace NoteTakingApp_UI.ViewModels
 {
-    public class ShellViewModel : Screen
+    public class Tag
     {
+        public Tag(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+
+    public class ListOfTags
+    {
+        public ListOfTags(string title)
+        {
+            Title = title;
+        }
+
+        public string Title { get; set; }
+        public List<Tag> TagsList { get; set; } = new List<Tag>();
+
+        public void AddTag(Tag tag)
+        {
+            TagsList.Add(tag);
+        }
+    }
+    public class ShellViewModel : Conductor<object>
+    {
+        public ShellViewModel()
+        {
+            ListOfTags listOfTags = new ListOfTags("Tags");
+            Tag personal = new Tag("Personal");
+            Tag work = new Tag("Work");
+        }
     }
 }
