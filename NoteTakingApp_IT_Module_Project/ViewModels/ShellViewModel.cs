bf8fea21ac5d2b826ea0adc9a426b76f5344d9c1
Iva Bondzheva <ivka@dsoft-bg.com>
2022-03-12 15:06:24 +0200

    Structure and practice with MVVM

---
+++ NoteTakingApp_IT_Module_Project/ViewModels/ShellViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoteTakingApp_UI.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        private string _firstName = "Tim";
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+    }
+}
