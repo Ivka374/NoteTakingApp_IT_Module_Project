e3947b5669f2626e1cec16222a152f679605480b
Iva Bondzheva <ivka@dsoft-bg.com>
2022-03-14 01:28:56 +0200

    Tweaked toggleable buttons and added exaples of music to have them available in the meantime

--- NoteTakingApp_IT_Module_Project/ViewModels/MusicViewModel.cs
+++ NoteTakingApp_IT_Module_Project/ViewModels/MusicViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Manufaktura.Controls.Model;
+using Manufaktura.Music.Model;
+using Manufaktura.Music.Model.MajorAndMinor;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +9,45 @@ namespace NoteTakingApp_UI.ViewModels
 {
     class MusicViewModel : ViewModel
     {
+        private Score data;
+
+        public Score Data
+        {
+            get { return data; }
+            set { data = value; OnPropertyChanged(() => Data); }
+        }
+
+
+        //for rerference
+        public void LoadTestData()
+        {
+            Score score = Score.CreateOneStaffScore(Clef.Treble, new MajorScale(Step.C, false));
+            score.FirstStaff.Elements.Add(new Note(Pitch.C5, RhythmicDuration.Quarter));
+            score.FirstStaff.Elements.Add(new Note(Pitch.B4, RhythmicDuration.Quarter));
+            score.FirstStaff.Elements.Add(new Note(Pitch.C5, RhythmicDuration.Half));
+            score.FirstStaff.Elements.Add(new Barline());
+            Data = score;
+
+            Staff secondStaff = new Staff();
+            secondStaff.Elements.Add(Clef.Treble);
+            secondStaff.Elements.Add(new Key(0));
+            secondStaff.Elements.Add(new Note(Pitch.G4, RhythmicDuration.Whole));
+            secondStaff.Elements.Add(new Barline());
+            score.Staves.Add(secondStaff);
+
+            score.Staves.Add(new Staff());
+            score.ThirdStaff.Elements.Add(Clef.Tenor);
+            score.ThirdStaff.Elements.Add(new Key(0));
+            score.ThirdStaff.Elements.Add(new Note(Pitch.D4, RhythmicDuration.Half));
+            score.ThirdStaff.Elements.Add(new Note(Pitch.E4, RhythmicDuration.Half));
+            score.ThirdStaff.Elements.Add(new Barline());
+
+            score.Staves.Add(new Staff());
+            score.Staves[3].Elements.Add(Clef.Bass);    //0-based index
+            score.Staves[3].Elements.Add(new Key(0));
+            score.Staves[3].Elements.Add(new Note(Pitch.G3, RhythmicDuration.Half));
+            score.Staves[3].Elements.Add(new Note(Pitch.C3, RhythmicDuration.Half));
+            score.Staves[3].Elements.Add(new Barline());
+        }
     }
 }
