a17c14ae2f760ccdb64208c5e14d8f1a7ef026dc
Atila Sarachoglu <atrupb@hotmail.com>
2022-03-15 19:50:25 +0200

    note content things

--- NoteTakingApp_IT_Module_Project/Views/AddAndEditNoteView.xaml.cs
+++ NoteTakingApp_IT_Module_Project/Views/AddAndEditNoteView.xaml.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using NoteTakingApp_UI.Models;
+using NoteTakingApp_UI.ViewModels;
 
 namespace NoteTakingApp_IT_Module_Project.Views
 {
@@ -23,6 +25,7 @@ namespace NoteTakingApp_IT_Module_Project.Views
             deleteMenuItem.AddHandler(MenuItem.ClickEvent, new RoutedEventHandler(deleteMenuItem_Click)); //delete button now has onclick event
             changeColourMenuItem.AddHandler(MenuItem.ClickEvent, new RoutedEventHandler(changeColourMenuItem_Click)); //change color now has onclick event
             addScoreMenuItem.AddHandler(MenuItem.ClickEvent, new RoutedEventHandler(addScoreMenuItem_Click)); //add score now has onclick event
+            this.PreviewKeyDown += new KeyEventHandler(AddAndEditNoteView_PreviewKeyDown); // window now detects keyboard input
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -41,6 +44,44 @@ namespace NoteTakingApp_IT_Module_Project.Views
         {
             //ill need help with this i have no idea what it would look like on screen lol
         }
-
+        private void AddAndEditNoteView_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            //REMEMBER TO ADD A CHECK FOR TITLE AS WELL WHEN ITS IMPLEMENTED!!!
+            //REMEMBER TO ADD A CHECK FOR TITLE AS WELL WHEN ITS IMPLEMENTED!!!
+            //REMEMBER TO ADD A CHECK FOR TITLE AS WELL WHEN ITS IMPLEMENTED!!!
+            if(noteTextBox.Text == "")
+            {
+                if (e.Key == Key.Escape)
+                {
+                    this.Close();
+                }
+            }
+            //saves a new note
+            else
+            {
+                if (e.Key == Key.Escape)
+                {
+                    
+                    NoteModel note = new NoteModel();
+                    NoteContentModel noteContents = new NoteContentModel();
+                    noteContents.TextContent = noteTextBox.Text;
+                    //noteContents.MusicContent = ???;
+                    //note.Title = ???;
+                    //note.ThemeName = ???;
+                    //note.NoteTags = ???;
+                    note.Content = noteContents;
+                    if(favoriteButton.IsChecked == true)
+                    {
+                        note.IsFavourite = true;
+                    }
+                    else
+                    {
+                        note.IsFavourite = false;
+                    }
+                    //how to add this to the HomePageViewModel list var? shit man this aint nothing like unity ;-;
+                    this.Close();
+                }
+            }
+        }
     }
 }
