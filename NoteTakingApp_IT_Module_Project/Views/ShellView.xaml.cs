78c97d9f7140463b4d6bb2d6e8e51f9b6cfc893b
Atila Sarachoglu <atrupb@hotmail.com>
2022-03-16 18:24:07 +0200

    wfada

--- NoteTakingApp_IT_Module_Project/Views/ShellView.xaml.cs
+++ NoteTakingApp_IT_Module_Project/Views/ShellView.xaml.cs
@@ -4,6 +4,9 @@ using System.Windows;
 using System.Diagnostics;
 using System;
 using System.Windows.Controls;
+using System.Windows.Input;
+using Caliburn.Micro;
+using NoteTakingApp_IT_Module_Project.ViewModels;
 
 namespace NoteTakingApp_UI.Views
 {
@@ -12,11 +15,13 @@ namespace NoteTakingApp_UI.Views
     /// </summary>
     public partial class ShellView : Window
     {
+        WindowManager windowManager;
         bool isMaximized = false;
         public ShellView()
         {
             InitializeComponent();
             Close.AddHandler(Button.ClickEvent, new RoutedEventHandler(Close_Click)); //this has to be here for some reason, just leave it be
+            CreateNote.AddHandler(Button.ClickEvent, new RoutedEventHandler(CreateNote_Click)); //this has to be here for some reason, just leave it be
             //continuing music example
         }
 
@@ -29,6 +34,11 @@ namespace NoteTakingApp_UI.Views
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            this.DragMove();
+        }
+        private void Close_Click(object sender, RoutedEventArgs e)
         {
             Environment.Exit(0);
         }
@@ -48,6 +58,10 @@ namespace NoteTakingApp_UI.Views
         private void Minimize_Click(object sender, RoutedEventArgs e)
         {
             MainWindow.WindowState = WindowState.Minimized;
+        }
+        private void CreateNote_Click(object sender, RoutedEventArgs e)
+        {
+            windowManager.ShowDialogAsync(new AddAndEditNoteViewModel());
         }
     }
 }
